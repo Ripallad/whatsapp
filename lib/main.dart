3af8a597cbf48698ed611fb7369b1321716a4ffb
Ripallad <ripallad123@gmail.com>
2024-01-24 12:58:39 +0530

    dna

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:whatsapp/firebase_options.dart';
-import 'package:whatsapp/screen/addnewuser.dart';
 import 'package:whatsapp/screen/splash.dart';
 
 void main() async {
